ad16a673e16216c957d925ea42b90d920ea28bc3
Paddu08 <100520058+Paddu08@users.noreply.github.com>
2023-05-15 20:38:10 +0530

    Delete todoh2 2 directory

--- todoh2 2/src/main/java/com/example/todoh2/Repository/TodoItemRepository/TodoItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todoh2.Repository.TodoItemRepository;
-
-// TodoItemRepository.java
-
-import com.example.todoh2.Todoitem.TodoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
-}
